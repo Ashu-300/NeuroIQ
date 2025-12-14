94c5da7afdc66837d2f6ead1dd9a782027fba353
Ashu <ashutoshnigam300@gmail.com>
2025-12-14 09:24:52 +0530

    generate question from text input for single unit api implemented in question bank service

--- questionbank/src/dto/dto.go
+++ questionbank/src/dto/dto.go
@@ -17,3 +17,16 @@ type Claim struct {
 type Questions struct {
 	QuestionList	[]models.Question		`json:"questions" bson:"questions validate:"required"`
 }
+
+type LlmRequestBody struct {
+	Subject				string			`json:"subject"`
+	UnitSyllabus    	string			`json:"unit_syllabus"`
+	Num3Marks      		int				`json:"num_3marks"`
+	Num4Marks     		int				`json:"num_4marks"`
+	Num10Marks  		int				`json:"num_5marks"`
+}
+
+type LlmResponse struct {
+	Sucess 		bool	`json:"success"`
+	Questions	string	`json:"questions"`
+}
--- questionbank/src/routes/routes.go
+++ questionbank/src/routes/routes.go
@@ -13,6 +13,7 @@ func SetupQuestionbankRoutes() chi.Router{
 	router.Group(func(r chi.Router){
 		r.Use(middleware.AuthMiddleware)
 		r.Post("/register/questionset" , controller.RegisterQuestionSet)
+		r.Post("/generate/question/singleunit" , controller.GenrateQuestionFromText)
 	})
 
 	return router
